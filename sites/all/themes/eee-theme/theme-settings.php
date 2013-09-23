a93e675d046bd7ae9ebf413c5fc7797ac4268b3a
Pomin Wu <pomin5@gmail.com>
2013-09-23 14:20:55 +0800

    backup from prod site again after a recent fix
    fix slider font

--- sites/all/themes/eee-theme/theme-settings.php
+++ sites/all/themes/eee-theme/theme-settings.php
@@ -2,43 +2,43 @@
 
 /**
  * @file
- * Theme settings for the theme721
+ * Theme settings for the eee-theme
  */
-function theme721_form_system_theme_settings_alter(&$form, &$form_state) {
+function eee-theme_form_system_theme_settings_alter(&$form, &$form_state) {
 
   /**
    * Breadcrumb settings
    */
-  $form['theme721_breadcrumb'] = array(
+  $form['eee-theme_breadcrumb'] = array(
     '#type' => 'fieldset',
     '#title' => t('Breadcrumb'),
   );
   
-  $form['theme721_breadcrumb']['theme721_breadcrumb_display'] = array(
+  $form['eee-theme_breadcrumb']['eee-theme_breadcrumb_display'] = array(
    '#type' => 'select',
    '#title' => t('Display breadcrumb'),
-   '#default_value' => theme_get_setting('theme721_breadcrumb_display'),
+   '#default_value' => theme_get_setting('eee-theme_breadcrumb_display'),
    '#options' => array(
      'yes' => t('Yes'),
      'no' => t('No'),
    ),
   );
   
-  $form['theme721_breadcrumb']['theme721_breadcrumb_hideonlyfront'] = array(
+  $form['eee-theme_breadcrumb']['eee-theme_breadcrumb_hideonlyfront'] = array(
     '#type' => 'checkbox',
     '#title' => t('Hide the breadcrumb if the breadcrumb only contains the link to the front page.'),
-    '#default_value' => theme_get_setting('theme721_breadcrumb_hideonlyfront'),
+    '#default_value' => theme_get_setting('eee-theme_breadcrumb_hideonlyfront'),
   );
   
-  $form['theme721_breadcrumb']['theme721_breadcrumb_showtitle'] = array(
+  $form['eee-theme_breadcrumb']['eee-theme_breadcrumb_showtitle'] = array(
     '#type' => 'checkbox',
     '#title' => t('Show page title on breadcrumb.'),
-    '#default_value' => theme_get_setting('theme721_breadcrumb_showtitle'),
+    '#default_value' => theme_get_setting('eee-theme_breadcrumb_showtitle'),
   );
 
-  $form['theme721_breadcrumb']['theme721_breadcrumb_separator'] = array(
+  $form['eee-theme_breadcrumb']['eee-theme_breadcrumb_separator'] = array(
     '#type' => 'textfield',
     '#title' => t('Breadcrumb separator'),
-    '#default_value' => theme_get_setting('theme721_breadcrumb_separator'),
+    '#default_value' => theme_get_setting('eee-theme_breadcrumb_separator'),
   );
 }
